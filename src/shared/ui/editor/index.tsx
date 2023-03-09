ee12b228bda78076f5f330ce3d46fdb02eda8d89
dimicuss <dimicuss@gmail.com>
2023-03-09 17:52:51 +0300

    Add prosemirror editor

--- src/shared/ui/editor/index.tsx
+++ src/shared/ui/editor/index.tsx
@@ -1,4 +1,9 @@
 import {useEffect, useRef} from "react"
+import {schema} from 'prosemirror-schema-basic'
+import {EditorState} from "prosemirror-state"
+import {EditorView} from "prosemirror-view"
+import {keymap} from "prosemirror-keymap"
+import {splitBlock} from "prosemirror-commands"
 
 
 
@@ -6,7 +11,12 @@ export const Editor = () => {
   const ref = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
-    console.log(ref.current)
+    new EditorView(ref.current, {
+      state: EditorState.create({schema}),
+      plugins: [
+        keymap({'Enter': splitBlock})
+      ],
+    })
   }, [])
 
   return <div ref={ref}></div>
