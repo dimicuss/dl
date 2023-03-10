29c66289c4a4fc4121323da4348d8da0fdcd5bdf
dimicuss <dimicuss@gmail.com>
2023-03-10 13:46:37 +0300

    Add base scheme

--- src/shared/ui/editor/index.tsx
+++ src/shared/ui/editor/index.tsx
@@ -1,23 +1,117 @@
 import {useEffect, useRef} from "react"
-import {schema} from 'prosemirror-schema-basic'
 import {EditorState} from "prosemirror-state"
 import {EditorView} from "prosemirror-view"
 import {keymap} from "prosemirror-keymap"
-import {splitBlock} from "prosemirror-commands"
+import {undo, redo, history} from "prosemirror-history"
+import {DOMOutputSpec, Schema} from "prosemirror-model"
 
+const divEl: DOMOutputSpec = ['div', {class: 'Super'}, 0]
 
+const initialState = {
+  doc: {
+    type: "doc",
+    content: [
+      {
+        type: "super_expression",
+        content: [
+          {
+            type: 'expression',
+            content: [
+              {
+                type: 'entity',
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  selection: {
+    type: "text",
+    anchor: 0,
+    head: 0
+  }
+}
 
 export const Editor = () => {
   const ref = useRef<HTMLDivElement | null>(null)
+  const editorRef = useRef<EditorView | null>(null)
+
 
   useEffect(() => {
-    new EditorView(ref.current, {
-      state: EditorState.create({schema}),
-      plugins: [
-        keymap({'Enter': splitBlock})
-      ],
+    const view = new EditorView(ref.current, {
+      state: EditorState.fromJSON({
+        schema: new Schema({
+          nodes: {
+            'doc': {
+              content: '(super_expression | text)*'
+            },
+            'super_expression': {
+              content: '(expression | log_operator | l_brace | r_brace | text)+',
+              inline: true,
+              toDOM: () => divEl
+            },
+            'expression': {
+              content: '(entity | operator | value | text)+',
+              inline: true,
+              toDOM: () => divEl
+            },
+            'log_operator': {
+              atom: true,
+              inline: true,
+              toDOM: () => divEl
+            },
+            'l_brace': {
+              atom: true,
+              inline: true,
+              toDOM: () => divEl
+            },
+            'r_brace': {
+              atom: true,
+              inline: true,
+              toDOM: () => divEl
+            },
+            'entity': {
+              atom: true,
+              inline: true,
+              toDOM: () => divEl
+            },
+            'operator': {
+              atom: true,
+              inline: true,
+              toDOM: () => divEl
+            },
+            'value': {
+              content: 'text*',
+              inline: true,
+              toDOM: () => divEl
+            },
+            'text': {}
+          }
+        }),
+        plugins: [
+          history(),
+          keymap({
+            'Mod-z': undo,
+            'Mod-y': redo
+          })
+        ],
+      }, initialState),
+      dispatchTransaction(t) {
+        const newState = view.state.apply(t)
+        console.log(newState.toJSON())
+        view.updateState(newState)
+      }
     })
+
+    editorRef.current = view
+
+    return () => {
+      editorRef.current?.destroy()
+    }
   }, [])
+
+
 
   return <div ref={ref}></div>
 }
